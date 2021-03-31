98bc5d48abaa26e9eaaa873a0b768eceed6c4d75
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-03-31 05:42:22 +0530

    Install and add the firebase SDK

--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from 'firebase/app';
+import 'firebase/firestore'; //inport sepecifiic firestore which we need
 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2YG3AaUbzUQWwTrd8Zvl3ROGyuIhTueA",
+  authDomain: "cart-web-app-8bbe2.firebaseapp.com",
+  projectId: "cart-web-app-8bbe2",
+  storageBucket: "cart-web-app-8bbe2.appspot.com",
+  messagingSenderId: "116196790097",
+  appId: "1:116196790097:web:7eac01db1124170c6f8d70"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
